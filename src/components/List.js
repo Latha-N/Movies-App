62900c55a6370d45c0d2b1f032d23fb8b5d5910f
latha n <latha9510@gmail.com>
2020-07-04 18:55:05 +0530

    new one

--- src/components/List.js
+++ src/components/List.js
@@ -10,7 +10,7 @@ class Listing extends React.Component{
         }
     }
     componentDidMount(){
-        axios.get('https://api.themoviedb.org/3/movie/550?api_key=06b4380c36f00a2d78cd89c4b56c0512')
+        axios.get()
         .then(response=>{
             const movies=response.data
             console.log('movies',movies)
